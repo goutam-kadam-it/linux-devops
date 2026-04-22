cec8c6795765cc989f3ba03f58a77e4889017da6
Gautam Kadam <gautamkadam202@gmail.com>
2026-04-22 09:48:51 +0530

    added disk monitoring script

--- disk_check.sh
+++ disk_check.sh
@@ -1,8 +1,10 @@
+# This script checks disk usage and alerts if usage > 80%
+
 #!/bin/bash
 
 usage=$(df -h / | awk 'NR==2 {print $5}' | sed 's/%//')
 
-if [ $usage -gt 50 ]
+if [ $usage -gt 80 ]
 then
   echo "WARNING: Disk usage is high ($usage%)"
 else
